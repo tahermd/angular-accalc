9fffc7e9bec8e4e9c62514f422deb3a8b647374e
tahermd92 <taher.developer1@gmail.com>
2020-04-19 14:53:00 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HelloComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
